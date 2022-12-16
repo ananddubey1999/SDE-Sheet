9c119756ec2d41cdcafef54a70f32bb5913acdfd
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2022-12-17 00:28:02 +0530

    js

--- Frontend/Newton School/js/dom_prop_2.js
+++ Frontend/Newton School/js/dom_prop_2.js
@@ -1,3 +1,6 @@
+
+      //  <-------- 1st concept to add div in given paragraph by the use of js ------->
+
 // try to add some element with javaScript
 // create an element
 const divElement=document.createElement("div");
@@ -10,7 +13,10 @@ const paragraph = document.getElementById("content");
  paragraph.appendChild(divElement);
 
 
-//  2. now m going to print the so many button using for loop
+
+// <--------- 2. now m going to print the so many button in betwwen body using for loop ------->
+
+
 function  showbutton(){
      for(let i=1;i<=10;i++){
         // creat a button tag
@@ -28,4 +34,14 @@ function  showbutton(){
 showbutton();
 
 
-// `     `
+// <--------  when Button is Clicked I want to print Somthing  and count it in the cosonl ------->
+
+const buttonElement =  document.getElementById("btn");
+// whenever button is clicked do somthing
+// first Argument is the name of the event 
+// Second Argument is the fuction that should Execute once that event Happens
+
+buttonElement.addEventListener("click",function(){
+   //  this function will automatically be called when the button is clicked
+   console.log("Button Clicked");
+})
