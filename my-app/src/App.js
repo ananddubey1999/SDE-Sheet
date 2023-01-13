3838b0c504db8f88b9318f2f35a83072a26c58b6
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-01-13 22:08:23 +0530

    react

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// jsx
+import React from 'react';
+
+class App extends React.Component{
+
+  
+  render(){
+    // return Whatever you want to show on screen
+    return <h1>hello world</h1>
+  }
+
+}
+
+export default App;
